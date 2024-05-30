6fbdfdb676d041e34356d0468a8fde70a474c059
Jagadeesh Reddy <jagadeesh.jr5@gmail.com>
2024-05-30 14:58:38 +0530

    Commiting changes

--- answerengine.py
+++ answerengine.py
@@ -9,29 +9,32 @@ async def run_scraper(query, num_urls):
     return scraped_content, urls
 
 
+def answerdotengine():
+    try:
+        prompt  = str(input("Ask me! : "))
+        num_urls = 5
+        context_percentage = 0.5
+        start_time = time.time()
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+        scraped_content, urls = loop.run_until_complete(run_scraper(prompt, num_urls))
 
-try:
-    prompt  = str(input("Ask me! : "))
-    num_urls = 5
-    context_percentage = 0.5
-    start_time = time.time()
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
-    scraped_content, urls = loop.run_until_complete(run_scraper(prompt, num_urls))
+        chunks = create_chunks(scraped_content)
+        context = make_context(query=prompt, context=chunks, context_percentage=context_percentage)
+        end_time = time.time()  # Record end time
+        runtime = end_time - start_time
+        #print(f"Time taken to retreive the context: {runtime:.2f} seconds")
+        model = Model()
+        output = model.answer(query=prompt, context=context)
+        
+        print(output)
 
-    chunks = create_chunks(scraped_content)
-    context = make_context(query=prompt, context=chunks, context_percentage=context_percentage)
-    end_time = time.time()  # Record end time
-    runtime = end_time - start_time
-    print(f"Time taken to retreive the context: {runtime:.2f} seconds")
-    model = Model()
-    output = model.answer(query=prompt, context=context)
-    print(output)
+    except Exception as e:
 
-except Exception as e:
+       print(e)
 
-    print(Exception)
-
+if __name__ == "__main__":
+    answerdotengine()
 
 
 
