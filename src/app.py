710f4cee06b8c3f6b2320169e41914eb548a1070
Jagadeesh Reddy <jagadeesh.jr5@gmail.com>
2024-08-10 20:33:40 +0530

    Commiting changes

--- src/app.py
+++ src/app.py
@@ -218,4 +218,4 @@ if prompt := st.chat_input("Ask me!"):
         except Exception as e:
             #st.error("Error accessing the response content. Please check the response structure.")
             st.write("I'm sorry! I cannot answer the query at the moment. Try again later or choose another model.")
-            #st.write(e)
+            st.write(e)
