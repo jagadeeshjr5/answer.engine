cfbb230ea6fc84624ef1505b651911fbb05d3e39
Jagadeesh Reddy <jagadeesh.jr5@gmail.com>
2024-08-12 15:46:31 +0000

    Commiting changes

--- src/installplaywrightbrowser.py
+++ src/installplaywrightbrowser.py
@@ -1,26 +1,9 @@
-from setuptools import setup, find_packages
-from setuptools.command.install import install as _install
-from playwright.sync_api import sync_playwright
+# installplaywrightbrowser.py
+import subprocess
+import sys
 
-class InstallWithPlaywright(_install):
-    def run(self):
-        _install.run(self)
-        self.install_playwright()
+def install_playwright_browsers():
+    subprocess.check_call([sys.executable, '-m', 'playwright', 'install'])
 
-    def install_playwright(self):
-        print("Installing Playwright browsers...")
-        with sync_playwright() as p:
-            p.chromium.install()
-        print("Playwright browsers installed.")
-
-setup(
-    name="your_project_name",
-    version="0.1",
-    packages=find_packages(),
-    install_requires=[
-        'playwright',  # Add other dependencies here
-    ],
-    cmdclass={
-        'install': InstallWithPlaywright,
-    },
-)
+if __name__ == "__main__":
+    install_playwright_browsers()
