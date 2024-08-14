23e6ae2d92747562ad5939523b419c2bf3d1dff2
Jagadeesh Reddy <jagadeesh.jr5@gmail.com>
2024-08-14 19:28:59 +0530

    Commiting changes

--- src/app.py
+++ src/app.py
@@ -43,20 +43,21 @@ async def process_query(prompt, num_urls, context_percentage, model, history):
 st.set_page_config(
         page_title="answer.engine", page_icon=f"{urls['pageicon']}")
 
-@st.cache_resource
 def install_playwright():
-    """Ensure Playwright and its dependencies are installed."""
+    """Function to install Playwright browser dependencies."""
     if 'playwright_installed' not in st.session_state:
-        # Install Playwright and its browser dependencies
+        # Run installation only if it hasn't been marked as done in the session state
         subprocess.run(["playwright", "install"], check=True)
-        subprocess.run(["playwright", "install-deps"], check=True)
         st.session_state['playwright_installed'] = True
         st.write("Playwright installed.")
     else:
         st.write("Playwright installation already completed.")
 
-# Run this function at the start of your app
+# Place this at the start of your app to ensure it runs when the app is first loaded
 install_playwright()
+
+os.system('playwright install-deps')
+os.system('playwright install')
 
 answer_color = "#c32148"
 
